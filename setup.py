3d328639233fc10dedbc4516fe7a1c4c72d00177
Goblenus <goblenus@ro.ru>
2017-01-08 00:22:15 +0300

    (Add) Auto deploy to pypi

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ s = setup(
         'psutil'
     ],
     name='PyAccessPoint',
-    version='0.0.6',
+    version='0.0.7',
     description='Package to manage wifi hotspot on linux',
     long_description=long_description,
     license='GNU GPLv3',
