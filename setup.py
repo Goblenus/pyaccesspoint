c1967ece1ee3441f92ea929e3e5feeeb4acd183f
Goblenus <goblenus@ro.ru>
2017-01-07 20:48:53 +0300

    (Fix) Add convert README.md to rst format

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 s = setup(
     install_requires=[
@@ -7,8 +12,9 @@ s = setup(
         'psutil'
     ],
     name='PyAccessPoint',
-    version='0.0.4',
+    version='0.0.5',
     description='Package to manage wifi hotspot on linux',
+    long_description=long_description,
     license='GNU GPLv3',
     author='Anton Bautkin',
     author_email='antonbautkin@gmail.com',
