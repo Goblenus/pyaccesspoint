8e92c78712f4a02774c79daf43a596d8bb0880f6
Goblenus <goblenus@ro.ru>
2017-01-08 01:03:53 +0300

    (Fix) Rolled back setup.py with some fixes

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+import os
+
+if os.path.exists('README.rst'):
+    with open('README.rst') as readme_rst_file:
+        long_description = readme_rst_file.read()
+else:
+    long_description = 'No description'
 
 s = setup(
     install_requires=[
@@ -9,6 +16,7 @@ s = setup(
     name='PyAccessPoint',
     version='0.0.10',
     description='Package to manage wifi hotspot on linux',
+    long_description=long_description,
     license='GNU GPLv3',
     author='Anton Bautkin',
     author_email='antonbautkin@gmail.com',
