a3f1e35af0759da302166d16795af03960421db6
Goblenus <goblenus@ro.ru>
2017-01-10 23:05:59 +0300

    (Upd) README.rst updated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ s = setup(
         'psutil'
     ],
     name='PyAccessPoint',
-    version='0.2.0',
+    version='0.2.1',
     description='Package to manage wifi hotspot on linux',
     long_description=long_description,
     license='GNU GPLv3',
